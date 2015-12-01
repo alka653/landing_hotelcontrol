3ba49d810202c1bd5dd438f36413428df032930b
Adriann Sanchez <alka65@hotmail.com>
2015-12-01 18:37:02 -0500

    send email 2

--- suscribe/settings.py
+++ suscribe/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '70hd56+ui_x_x_c%f6tyoo)!g0t9a@(#6$h7!b_r=j%sz!0i3-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
