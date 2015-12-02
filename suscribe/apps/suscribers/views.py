21f9902b981c5e78a6aeb93c3c275f8f21cc1f93
Adriann Sanchez <alka65@hotmail.com>
2015-12-01 22:11:34 -0500

    new email add

--- suscribe/apps/suscribers/views.py
+++ suscribe/apps/suscribers/views.py
@@ -10,7 +10,7 @@ def home(request, *args):
 		form = SuscriberForm(request.POST)
 		if form.is_valid():
 			if form.save():
-				email = EmailMessage('Nuevo Registro', str('Nombre: '+request.POST['name']+'. Correo: '+request.POST['email']+'. Hotel: '+request.POST['hotel']), to = ['adriann.sanchez1@gmail.com'])
+				email = EmailMessage('Nuevo Registro', str('Nombre: '+request.POST['name']+'. Correo: '+request.POST['email']+'. Hotel: '+request.POST['hotel']), to = ['adriann.sanchez1@gmail.com', 'alka65@hotmail.com', 'laurentino-carranza@unipiloto.edu.co'])
 				email.send()
 				messages.add_message(request, 25, 'Exito en la inscripcion')
 			else:
